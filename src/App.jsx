3e571022f82a97b2b6e87c9aadb3a2667492f89c
Lilia <lilijusatre@gmail.com>
2024-02-15 00:27:39 -0600

    se creó la estructura del proyecto, maquetación

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faEye } from "@fortawesome/free-regular-svg-icons";
+
 export function App() {
   return (
-    <div>
-      <h1>markdown</h1>
-      <textarea id="editor"></textarea>
+    <div className="screen">
+      <section className="editor content">
+        <div className="toolbar">
+          <i>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </i>
+          <p>Editor</p>
+        </div>
+        <textarea id="editor"></textarea>
+      </section>
+      <section className="viewer content" id="preview">
+        <div className="toolbar">
+          <i>
+            <FontAwesomeIcon icon={faEye} />
+          </i>
+          <p>Previewer</p>
+        </div>
+      </section>
     </div>
   );
 }
